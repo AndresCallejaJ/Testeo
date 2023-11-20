d901eb0e1506e59ff3a2f18b15aa2a55757a4cf5
AndresCallejaJ <143973406+AndresCallejaJ@users.noreply.github.com>
2023-11-20 01:36:42 -0600

    Create Main Movie.kt

---
+++ Main Movie.kt
@@ -0,0 +1,9 @@
+import clases.*
+
+fun main() {
+	val scaryMovie = Movie(name: "Scary movie", gender: "Comedia", duration:66.77)
+	println(scaryMovie)
+	//println(scaryMovie.name)
+	scaryMovie. createAt = "2000"
+	println("fcha de creación: ${scaryMovie.createAt}")
+}
